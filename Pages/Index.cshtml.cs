4f6ca9ef69274061b6909c54ed9dd5e39f61f276
DimasB89 <belogurov89@gmail.com>
2023-02-01 16:58:48 +0100

    Main page functionality
    design aside, functionality of front page is implemented:
    Grabbing of events from db, sorting them by date and adding to two tables, past and upcoming events. shared links go to correct elements.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebEventManager.Data;
+using WebEventManager.Models;
 
 namespace WebEventManager.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        /*public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+        }*/
+
+        private readonly EMContext _context;
+        private DateTime _currentDateTime;
+        private List<Event> _upcomingEvents;
+        private List<Event> _pastEvents;
+
+        public IndexModel(EMContext context)
+        {
+            _context = context;
+            _currentDateTime = DateTime.Now;
+            _upcomingEvents = new List<Event>();
+            _pastEvents = new List<Event>();
         }
 
+        public IEnumerable<Event> GetUpcomingEvents { get; set; }
+        public IEnumerable<Event> GetPastEvents { get; set; }
+
+        
         public void OnGet()
         {
-
+            IEnumerable<Event> events = _context.Events.ToList();
+            foreach (Event e in events)
+            {
+                if(DateTime.Compare(e.DateTime, _currentDateTime) > 0)
+                {
+                    _upcomingEvents.Add(e);
+                }
+                else
+                {
+                    _pastEvents.Add(e);
+                }
+            }
+            GetUpcomingEvents = _upcomingEvents;
+            GetPastEvents = _pastEvents;
         }
     }
 }
