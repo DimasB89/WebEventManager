65d53398e62f09bd1a2f617a72c62925bdf42b2f
DimasB89 <belogurov89@gmail.com>
2023-02-13 00:14:30 +0100

    Event.Details code cleanup

--- Pages/Events/Details.cshtml.cs
+++ Pages/Events/Details.cshtml.cs
@@ -35,62 +35,46 @@ namespace WebEventManager.Pages.Events
 
         public IEnumerable<(string name, long id, bool isPerson, int actualID)> GetParticipants { get; set; }
 
+        
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if (id == null || _context.Events == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var evento = await _context.Events.FirstOrDefaultAsync(m => m.EventID == id);
-            if (evento == null)
+            Event = await _context.Events.FirstOrDefaultAsync(m => m.EventID == id);
+            if (Event == null)
             {
                 return NotFound();
             }
-            else
-            {
-                Event = evento;
-            }
 
-
-            List<Participant> participants = new List<Participant>();
-            
-            foreach (Attendance a in _context.Attendances.ToList())
-            {
-                if (a.EventID.Equals(Event.EventID))
-                {
-                    foreach (Participant p in _context.Participants.ToList())
-                    {
-                        if (a.ParticipantID.Equals(p.ParticipantID))
-                        {
-                            participants.Add(p);
-                        }
-                    }
-                }
-            }
-
+            List<Participant> participants = await _context.Attendances
+                .Where(a => a.EventID == Event.EventID)
+                .Select(a => _context.Participants.FirstOrDefault(p => p.ParticipantID == a.ParticipantID))
+                .ToListAsync();
 
             foreach (var participant in participants)
             {
-                foreach (PrivatePerson person in _context.PrivatePersons)
+                PrivatePerson person = _context.PrivatePersons.FirstOrDefault(p => p.PrivatePersonID == participant.ParticipantID);
+                if (person != null)
                 {
-                    if (person.PrivatePersonID.Equals(participant.ParticipantID))
-                    {
-                        _participants.Add((person.FullName, person.PersonalID, true, person.PrivatePersonID));
-                    }
+                    _participants.Add((person.FullName, person.PersonalID, true, person.PrivatePersonID));
                 }
-                foreach (Company company in _context.Companies)
+                else
                 {
-                    if (company.CompanyID.Equals(participant.ParticipantID))
+                    Company company = _context.Companies.FirstOrDefault(c => c.CompanyID == participant.ParticipantID);
+                    if (company != null)
                     {
                         _participants.Add((company.Name, company.RegistryNumber, false, company.CompanyID));
                     }
                 }
             }
-            GetParticipants = _participants;
 
+            GetParticipants = _participants;
             return Page();
         }
+
 
         public async Task<IActionResult> OnPostAsync()
         {
@@ -110,44 +94,26 @@ namespace WebEventManager.Pages.Events
             if (!ModelState.IsValid)
             {
                 ModelState.AddModelError("NewPrivatePerson.PersonalID", "Vale ID");
-
-                foreach (var value in ModelState.Values)
-                {
-                    if (value.Errors.Count > 0)
-                    {
-                        foreach (var error in value.Errors)
-                        {
-                            Console.WriteLine(error.ErrorMessage);
-                        }
-                    }
-                }
                 return Page();
             }
-            
 
+            var participant = selectedParticipantType switch
+            {
+                "PrivatePerson" => new Participant(NewPrivatePerson.FirstName, NewPrivatePerson.LastName, NewPrivatePerson.PersonalID),
+                "Company" => new Participant(NewCompany.Name, NewCompany.RegistryNumber, NewCompany.NumberOfParticipants),
+                _ => throw new ArgumentException("Invalid participant type")
+            };
 
-            //add participant
-            Participant participant = new Participant();
-            
-            if (selectedParticipantType == "PrivatePerson")
-            {
-                participant = new Participant(NewPrivatePerson.FirstName, NewPrivatePerson.LastName, NewPrivatePerson.PersonalID);
-            }
-            else if (selectedParticipantType == "Company")
-            {
-                participant = new Participant(NewCompany.Name, NewCompany.RegistryNumber, NewCompany.NumberOfParticipants);
-            }
             _context.Participants.Add(participant);
             await _context.SaveChangesAsync();
 
-            //add attendance
-            NewAttendance.ParticipantID = participant.ParticipantID;
-            Attendance attendance = new Attendance(NewAttendance.ParticipantID, Event.EventID, NewAttendance.PaymentMethod, NewAttendance.AdditionalInformation);
+            var attendance = new Attendance(participant.ParticipantID, Event.EventID, NewAttendance.PaymentMethod, NewAttendance.AdditionalInformation);
             _context.Attendances.Add(attendance);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Details", new { id = Event.EventID });
         }
+
 
     }
 }
