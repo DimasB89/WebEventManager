dded75448b82342ab62f50d81bf84b6943c9921d
DimasB89 <belogurov89@gmail.com>
2023-02-12 23:15:25 +0100

    Commented out the database seeding

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -41,14 +41,6 @@ namespace WebEventManager.Data
 
             var attendances = new Attendance[]
             {
-                /*new Attendance{ParticipantID=1, EventID=1, PaymentMethod = PaymentMethod.CreditCard, AdditionalInformation = "brings cake"},
-                new Attendance{ParticipantID=2, EventID=1, PaymentMethod = PaymentMethod.Cash, AdditionalInformation = "brings joy"},
-                new Attendance{ParticipantID=3, EventID=2, PaymentMethod = PaymentMethod.CreditCard, AdditionalInformation = "brings flowers"},
-                new Attendance{ParticipantID=4, EventID=2, PaymentMethod = PaymentMethod.Cash, AdditionalInformation = "brings fireworks"},
-                new Attendance{ParticipantID=5, EventID=3, PaymentMethod = PaymentMethod.CreditCard, AdditionalInformation = "brings fun"},
-                new Attendance{ParticipantID=6, EventID=3, PaymentMethod = PaymentMethod.Cash, AdditionalInformation = "brings happiness"},
-                new Attendance{ParticipantID=7, EventID=4, PaymentMethod = PaymentMethod.CreditCard, AdditionalInformation = "brings no errors"},
-                new Attendance{ParticipantID=8, EventID=4, PaymentMethod = PaymentMethod.Cash, AdditionalInformation = "brings jobs for everyone"},*/
                 new Attendance(1, 1, PaymentMethod.BankTransfer, "brings cake"),
                 new Attendance(2, 1, PaymentMethod.Cash, "brings joy"),
                 new Attendance(3, 2, PaymentMethod.BankTransfer, "brings flowers"),
