1a729cba0e780be4e61153685d0540f5d3cae945
DimasB89 <belogurov89@gmail.com>
2023-02-13 00:47:26 +0100

    added more tests

---
+++ Tests/Pages/Events/CreateEventTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebEventManager.Data;
+using WebEventManager.Models;
+using WebEventManager.Pages.Events;
+using Xunit;
+
+namespace WebEventManager.Tests.Pages.Events
+{
+    public class CreateEventTests
+    {
+        private readonly EMContext _context;
+
+        public CreateEventTests()
+        {
+            var options = new DbContextOptionsBuilder<EMContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new EMContext(options);
+        }
+
+        [Fact]
+        public async Task OnPostAsync_WhenEventIsValid_AddsEventToDatabase()
+        {
+            var createModel = new CreateModel(_context);
+            var eventToAdd = new Event
+            {
+                Name = "Test Event",
+                Place = "Test Place",
+                DateTime = DateTime.Now
+            };
+
+            createModel.Event = eventToAdd;
+
+            var result = await createModel.OnPostAsync();
+
+            Assert.IsType<RedirectToPageResult>(result);
+            Assert.Equal("../Index", (result as RedirectToPageResult).PageName);
+            Assert.Equal(1, _context.Events.Count());
+            Assert.Equal("Test Event", _context.Events.First().Name);
+            Assert.Equal("Test Place", _context.Events.First().Place);
+        }
+
+    }
+}
---
+++ Tests/Pages/Events/DeleteEventTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebEventManager.Data;
+using WebEventManager.Models;
+using Xunit;
+using WebEventManager.Pages.Events;
+
+namespace WebEventManager.Tests.Pages.Events
+{
+    public class DeleteEventTests
+    {
+        [Fact]
+        public async Task OnGetAsync_ReturnsNotFound_WhenIdIsNull()
+        {
+            // Arrange
+            var context = GetContext();
+            var model = new DeleteModel(context);
+
+            // Act
+            var result = await model.OnGetAsync(null);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task OnGetAsync_ReturnsNotFound_WhenEventNotFound()
+        {
+            // Arrange
+            var context = GetContext();
+            var model = new DeleteModel(context);
+
+            // Act
+            var result = await model.OnGetAsync(0);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<NotFoundResult>(result);
+        }
+
+
+        [Fact]
+        public async Task OnPostAsync_ReturnsNotFound_WhenIdIsNull()
+        {
+            // Arrange
+            var context = GetContext();
+            var model = new DeleteModel(context);
+
+            // Act
+            var result = await model.OnPostAsync(null);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<NotFoundResult>(result);
+        }
+
+
+        private EMContext GetContext()
+        {
+            var options = new DbContextOptionsBuilder<EMContext>()
+                .UseInMemoryDatabase(databaseName: "DeleteModelTests")
+                .Options;
+
+            return new EMContext(options);
+        }
+    }
+}
